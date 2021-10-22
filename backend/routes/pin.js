86c87e9f2be68acad8f8c4ad7518fc4c87d2e7ea
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2021-10-22 15:31:09 +0530

    Add files via upload

--- backend/routes/pin.js
+++ backend/routes/pin.js
@@ -1,1 +1,33 @@
+const router = require("express").Router();
 
+const Pin=require("../models/Pin")
+
+//create new pin
+
+router.post("/",async (req,res)=>{
+    console.log(req.body)
+    const newPin = new Pin(req.body);
+    try{
+        const savedpin =await newPin.save();
+        res.status(200).json(savedpin);
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//get all pins
+router.get("/",async (req,res)=>{
+    try{
+        
+        const pins = await Pin.find()
+        console.log("getting",pins)
+        res.send(pins)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+module.exports =router
